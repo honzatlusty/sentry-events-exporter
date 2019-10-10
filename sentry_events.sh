fb1a51f62a78ee2c3d62f66a5dc8a554dbd606f2
honza <honza@inuits.eu>
2019-10-10 11:16:45 +0200

    add code
    Signed-off-by: honza <honza@inuits.eu>

---
+++ sentry_events.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+token=$1
+sentry_url=$2
+hours_ago=$3
+timestamp=$(date +%s --date=" ${hours_ago} hours ago")
+timestamp_now=$(date +%s)
+organizations=$(curl --silent -H "Authorization: Bearer ${token}" ${sentry_url}/api/0/organizations/ | jq .[].slug -r)
+
+for organization in $organizations; do
+  projects=$(curl --silent -H "Authorization: Bearer ${token}" "${sentry_url}/api/0/organizations/${organization}/projects/" | jq .[].slug -r)
+  for project in $projects; do
+    events=$(curl --silent -H "Authorization: Bearer ${token}" "${sentry_url}/api/0/projects/${organization}/${project}/stats/?&since=${timestamp}&until=${timestamp_now}" | jq .[][1] | awk '{s+=$1} END {print s}')
+    echo "# HELP sentry_events_received_per_hour_${project} /api/0/projects/${organization}/${project}/stats/?&since=${timestamp}&until=${timestamp_now}
+# TYPE sentry_events_received_per_hour_${project} gauge
+sentry_events_received_per_hour_${project} ${events}"
+  done
+done
---
+++ sentry_events_exporter.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+type OptsHandler struct {
+	Token string
+	URL   string
+	Hours string
+}
+
+func (oh *OptsHandler) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "#Sentry Event Exporter for Prometheus V.1.0 : Path : %s \n \n", r.URL.Path[1:])
+	out, err := exec.Command("/bin/bash", "sentry_events.sh", oh.Token, oh.URL, oh.Hours).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "%s", out)
+}
+
+func usage() {
+	fmt.Print("Example usage: ./exporter -token <sentry api token> -url <sentry url> -hours <number of the past hours to search for events from>\n" +
+		"<token> - token for Sentry API                   - Mandatory\n" +
+		"<url>   - Sentry url                             - Optional - defaults to \"http://localhost\"\n" +
+		"<hours> - how many hours to look back for events - Optional - defaults to 1\n")
+}
+
+func main() {
+	token := flag.String("token", "", "token for Sentry API")
+	if *token == "" {
+		usage()
+		os.Exit(2)
+	}
+	url := flag.String("url", "http://localhost", "Sentry url")
+	hoursAgo := flag.String("hours", "1", "how many hours to look back for events")
+	flag.Parse()
+	myFilesHandler := &OptsHandler{Token: *token, URL: *url, Hours: *hoursAgo}
+	http.HandleFunc("/metrics", myFilesHandler.handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+/*Based on https://medium.com/@jomzsg/prometheus-wildlfy-exporter-using-shell-script-and-golang-cf7ec4179c0b*/
