c34693daa033d8ea47903898e2e6ca67c2ec9775
honza <honza@inuits.eu>
2019-10-11 12:05:13 +0200

    use labels properly
    Signed-off-by: honza <honza@inuits.eu>

--- sentry_events.sh
+++ sentry_events.sh
@@ -7,13 +7,14 @@ timestamp=$(date +%s --date=" ${hours_ago} hours ago")
 timestamp_now=$(date +%s)
 organizations=$(curl --silent -H "Authorization: Bearer ${token}" ${sentry_url}/api/0/organizations/ | jq .[].slug -r)
 
+echo "# HELP sentry_events_received_per_hour_total /api/0/projects/<organization>/<project>/stats/?&since=${timestamp}&until=${timestamp_now}
+# TYPE sentry_events_received_per_hour_total gauge"
+
 for organization in $organizations; do
   projects=$(curl --silent -H "Authorization: Bearer ${token}" "${sentry_url}/api/0/organizations/${organization}/projects/" | jq .[].slug -r)
   for project in $projects; do
     events=$(curl --silent -H "Authorization: Bearer ${token}" "${sentry_url}/api/0/projects/${organization}/${project}/stats/?&since=${timestamp}&until=${timestamp_now}" | jq .[][1] | awk '{s+=$1} END {print s}')
     project_sanitized=$(echo $project | tr ' -.' '_')
-    echo "# HELP sentry_events_received_per_hour_${project_sanitized} /api/0/projects/${organization}/${project_sanitized}/stats/?&since=${timestamp}&until=${timestamp_now}
-# TYPE sentry_events_received_per_hour_${project_sanitized} gauge
-sentry_events_received_per_hour_${project_sanitized} ${events}"
+    echo "sentry_events_received_per_hour_total{project=\"${project_sanitized}\"} ${events}"
   done
 done
